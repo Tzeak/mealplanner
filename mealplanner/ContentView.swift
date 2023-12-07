f3baa171c172bae21867ea1bc25fcc34a5603099
rramankutty-sq <63818314+rramankutty-sq@users.noreply.github.com>
2023-12-07 11:41:10 -0800

    Add ingredient adder

--- mealplanner/ContentView.swift
+++ mealplanner/ContentView.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
-struct Ingredient: Identifiable {
-    var id = UUID()
+import Foundation
+
+
+struct Ingredient: Identifiable, Decodable {
+    var id = UUID()  // This is for Identifiable
     var name: String
-    var quantity: Int // Changed to an integer
-    var unit: String  // Added unit property
+    var quantity: Int
+    var unit: String
+
+    // Define CodingKeys if the JSON keys don't match your struct's property names
+    enum CodingKeys: String, CodingKey {
+        case name
+        case quantity
+        case unit
+    }
 }
+
 
 
 struct Meal: Identifiable {
@@ -38,8 +49,53 @@ enum MealType: String {
     case dinner = "Dinner"
 }
 
+struct APIResponse: Decodable {
+    let id: String
+    let object: String
+    let created: Int
+    let model: String
+    let choices: [Choice]
+}
+
+struct Choice: Decodable {
+    let index: Int
+    let message: Message
+    let finish_reason: String
+}
+
+struct Message: Decodable {
+    let role: String
+    let content: String  // This contains the JSON string of ingredients
+}
+
+struct IngredientsResponse: Decodable {
+    let ingredients: [Ingredient]
+}
+struct BackgroundTapModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .onTapGesture {
+                hideKeyboard()
+            }
+    }
+
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+extension View {
+    func dismissKeyboardOnBackgroundTap() -> some View {
+        self.modifier(BackgroundTapModifier())
+    }
+}
+
+
+
+
+
 struct DayListView: View {
-    @State var days: [Day] // Your days data
+    @Binding var days: [Day] // Your days data
     @State private var showSelectedMeals = false
 
     var body: some View {
@@ -50,7 +106,7 @@ struct DayListView: View {
                         Text($day.name.wrappedValue)
                     }
                 }
-                .navigationTitle("Days of the Week")
+                .navigationTitle("Meal Planner")
 
                 VStack {
                     Spacer() // Pushes the button to the bottom
@@ -200,11 +256,149 @@ struct MealItemView: View {
     }
 }
 
+struct RecipeAddView: View {
+    @Binding var meals: [Meal]
+    @State private var recipeName: String = ""
+    @State private var recipeDetails: String = ""
+    @State private var selectedMealType: MealType = .breakfast
 
+    var onSave: (Meal) -> Void
+
+    var body: some View {
+        VStack {
+            TextField("Recipe Name", text: $recipeName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            TextEditor(text: $recipeDetails)
+                .border(Color.gray, width: 1)
+                .padding()
+
+            Picker("Meal Type", selection: $selectedMealType) {
+                Text(MealType.breakfast.rawValue).tag(MealType.breakfast)
+                Text(MealType.lunch.rawValue).tag(MealType.lunch)
+                Text(MealType.dinner.rawValue).tag(MealType.dinner)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            Button("Save") {
+                        parseIngredients(from: recipeDetails) { ingredients in
+                            let newMeal = Meal(name: recipeName, ingredients: ingredients, selectedItem: false, type: selectedMealType)
+                            onSave(newMeal)  // This now happens after the ingredients are fetched
+                        }
+                    }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .background(Color.red.opacity(0.1).edgesIgnoringSafeArea(.all)
+            .dismissKeyboardOnBackgroundTap())
+        .padding()
+        
+
+        
+    }
+
+    func parseIngredients(from details: String, completion: @escaping ([Ingredient]) -> Void) {
+        let requestBody: [String: Any] = [
+                "model": "gpt-3.5-turbo-1106",
+                "response_format": ["type": "json_object"],
+                "messages": [
+                    ["role": "system", "content": "You are a recipe and ingredient transcriber. You transcribe recipe text or ingredient lists into JSON. The JSON has the attributes of name (the name of the ingredient), quantity, and unit. Quantities are always integers, never strings. When no unit type is provided by the specified text, use the ingredient name in the relevant singular or plural form to associate with the attribute 'unit'. An example JSON object looks like the following: {'ingredients':[{'name':'onion','quantity':'2','unit':'onions'}]}. Do not provide any special characters, no '\n' or other escape characters. You may use Emojis in the name or the unit only. When no recipe is detected, you return an empty JSON object."],
+                    ["role": "user", "content": details]
+                ]
+            ]
+
+        guard let url = URL(string: "https://api.openai.com/v1/chat/completions") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("Bearer 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", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: requestBody)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let httpResponse = response as? HTTPURLResponse {
+                print("HTTP Status Code: \(httpResponse.statusCode)")
+            }
+
+            if let data = data, let responseText = String(data: data, encoding: .utf8) {
+                print("Response Text: \(responseText)")
+            } else {
+                print("No response text.")
+            }
+
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                completion([])
+            } else {
+                // Attempt to decode the response
+                if let response = try? JSONDecoder().decode(APIResponse.self, from: data ?? Data()) {
+                    if let firstChoice = response.choices.first {
+                        let content = firstChoice.message.content
+
+                        if let contentData = content.data(using: .utf8),
+                           let ingredientsResponse = try? JSONDecoder().decode(IngredientsResponse.self, from: contentData) {
+                            let ingredients = ingredientsResponse.ingredients
+                            DispatchQueue.main.async {
+                                completion(ingredients)
+                                print("Success!")
+                            }
+                        } else {
+                            print("Failed to decode the ingredients from content.")
+                            completion([])
+                        }
+                    } else {
+                        print("No choices available in the response.")
+                        completion([])
+                    }
+                } else {
+                    print("Failed to decode APIResponse. Response Text: \(String(describing: String(data: data ?? Data(), encoding: .utf8)))")
+                    completion([])
+                }
+            }
+        }.resume()
+    }
+
+    func parseResponseIntoIngredients(_ text: String) -> [Ingredient] {
+        // Implement your parsing logic based on the expected structure of the response
+        var ingredients = [Ingredient]()
+        print("Ingredients: \(ingredients)")
+        return ingredients
+    }
+}
 
 struct ContentView: View {
+    @State var days = sampleDays
+    @State var meals = [Meal]() // Assuming this is your meals array
+    
     var body: some View {
-        DayListView(days: sampleDays)
+        TabView {
+            DayListView(days: $days)
+                .tabItem {
+                    Label("Week", systemImage: "calendar")
+                }
+            RecipeAddView(meals: $meals) { newMeal in
+                meals.append(newMeal)
+                addMealToDays(newMeal)
+            }
+            .tabItem {
+                Label("Add Recipe", systemImage: "plus")
+            }
+            SelectedMealsView(days: days)
+                .tabItem {
+                    Label("Shopping List", systemImage: "cart")
+                }
+            
+            
+        }
+    }
+    
+    func addMealToDays(_ meal: Meal) {
+        for index in days.indices {
+            days[index].meals.append(meal)
+        }
     }
 }
 
